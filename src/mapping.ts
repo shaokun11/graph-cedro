398e6d12cf0146e47919b025f8c5dd4d80dfe974
shaokun <shaokuning@gmail.com>
2022-11-10 11:07:12 +0800

    恢复id字段为本来字段,gql无法通过

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,16 +1,17 @@
-import {Borrow, Deposit, Withdraw} from "../generated/Server/Server";
-import {BorrowEntity, BorrowUsersEntity, DepositEntity, WithdrawEntity} from "../generated/schema";
+import {Borrow, Deposit, Repay, Withdraw} from "../generated/Server/Server";
+import {BorrowEntity, BorrowUsersEntity, DepositEntity, RepayEntity, WithdrawEntity} from "../generated/schema";
 import {BigInt} from "@graphprotocol/graph-ts";
 
 export function handleBorrow(event: Borrow): void {
     let id = event.transaction.hash.toHex()
     let entity = new BorrowEntity(id)
-    entity.amount = event.params.amount
+    entity.debtAmount = event.params.debtAmount
+    entity.tokenAmount = event.params.tokenAmount
     entity.block = event.block.number
     entity.timestamp = event.block.timestamp
     // @ts-ignore
     entity.chainId = BigInt.fromI32(event.params.chainId as i32)
-    entity.clientId = event.params.clientId
+    entity.key = event.params.id.toHex()
     entity.user = event.params.user.toHex()
     entity.save()
 
@@ -36,10 +37,11 @@ export function handleBorrow(event: Borrow): void {
 export function handleDeposit(event: Deposit): void {
     let id = event.transaction.hash.toHex()
     let entity = new DepositEntity(id)
-    entity.amount = event.params.amount
+    entity.tokenAmount = event.params.tokenAmount
+    entity.ceAmount = event.params.ceAmount
     // @ts-ignore
     entity.chainId = BigInt.fromI32(event.params.chainId as i32)
-    entity.clientId = event.params.clientId
+    entity.key = event.params.id.toHex()
     entity.user = event.params.user.toHex()
     entity.timestamp = event.block.timestamp
     entity.block = event.block.number
@@ -50,11 +52,25 @@ export function handleDeposit(event: Deposit): void {
 export function handleWithdraw(event: Withdraw): void {
     let id = event.transaction.hash.toHex()
     let entity = new WithdrawEntity(id)
-    entity.amount = event.params.amount
-    entity.share = event.params.share
+    entity.tokenAmount = event.params.tokenAmount
+    entity.ceAmount = event.params.ceAmount
     // @ts-ignore
     entity.chainId = BigInt.fromI32(event.params.chainId as i32)
-    entity.clientId = event.params.clientId
+    entity.key = event.params.id.toHex()
+    entity.user = event.params.user.toHex()
+    entity.timestamp = event.block.timestamp
+    entity.block = event.block.number
+    entity.save()
+}
+export function handleRepay(event: Repay): void {
+    let id = event.transaction.hash.toHex()
+    let entity = new RepayEntity(id)
+    entity.tokenAmount = event.params.tokenAmount
+    entity.ceAmount = event.params.ceAmount
+    entity.debtAmount = event.params.debtAmount
+    // @ts-ignore
+    entity.chainId = BigInt.fromI32(event.params.chainId as i32)
+    entity.key = event.params.id.toHex()
     entity.user = event.params.user.toHex()
     entity.timestamp = event.block.timestamp
     entity.block = event.block.number
