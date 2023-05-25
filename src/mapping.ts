de107bc8bc6e5f37c2819509e49c5f7b51a9bf34
shaokun <605416017@qq.com>
2023-05-25 12:01:24 +0800

    update to new contract

--- src/mapping.ts
+++ src/mapping.ts
@@ -12,23 +12,22 @@ import {
 } from "../generated/schema";
 import {Address, BigInt, Bytes, ethereum} from "@graphprotocol/graph-ts";
 
-
-let serverAddress = Address.fromString('0x2381401c1D413d80990B6b08beB829B195EBA616')
-let serverContract = Server.bind(serverAddress)
-let rootChainId = BigInt.fromString("10102")
+let CORE_ADDRESS = Address.fromString('0x01a2AFD378253860B833d1Ca25131EA47b968325')
+let serverContract = Server.bind(CORE_ADDRESS)
+let rootChainId = "0x424e420000000000000000000000000000000000000000000000000000000000"
+let previous_communicate_count = "269493"
 
 function updateAPY(event: ethereum.Event, key: Bytes, action: string, isNative: boolean): void {
     if (action != "LIQUIDATE") {
         // update apy
-        let k = event.transaction.hash.toHex()
+        let k = event.transaction.hash.toHexString()
         let entity = new ReserveEntity(k)
-        let res = serverContract.try_reserves(key)
-        //log.info("--------------key--{}--", [key.toHex()]);
+        let res = serverContract.try_pools(key)
         if (!res.reverted) {
             entity.currentRatio = res.value.value1
             entity.interestRate = res.value.value0
             entity.action = action
-            entity.key = key.toHex()
+            entity.key = key.toHexString()
             entity.timestamp = event.block.timestamp
             entity.block = event.block.number
             entity.save()
@@ -46,7 +45,7 @@ function updateAPY(event: ethereum.Event, key: Bytes, action: string, isNative: 
         summaryEntity.nonCrossCount = BigInt.fromString("0")
         summaryEntity.repayCount = BigInt.fromString("0")
         summaryEntity.borrowCount = BigInt.fromString("0")
-        summaryEntity.totalCount = BigInt.fromString("143574")
+        summaryEntity.totalCount = BigInt.fromString(previous_communicate_count)
         summaryEntity.liquidateCount = BigInt.fromString("0")
         summaryEntity.save()
         summaryEntity = SummaryEntity.load("1");
@@ -74,27 +73,28 @@ function updateAPY(event: ethereum.Event, key: Bytes, action: string, isNative: 
 }
 
 export function handleLiquidate(event: Liquidate): void {
-    let entity = new LiquidateEntity(event.transaction.hash.toHex())
-    entity.user = event.params.user.toHex()
-    entity.liquidator = event.params.liquidator.toHex()
-    entity.ceCollateralAmount = event.params.ceCollateralAmount
-    entity.debtId = event.params.debtId.toHex()
-    entity.collateralId = event.params.collateralId.toHex()
-    entity.debtAmount = event.params.debtAmount
-    entity.ceDebtAmount = event.params.ceDebtAmount
+    let entity = new LiquidateEntity(event.transaction.hash.toHexString())
+    entity.user = event.params.user.toHexString()
+    entity.liquidator = event.params.liquidator.toHexString()
+    entity.collateralAmount = event.params.collateralAmount
+    entity.debtId = event.params.debtId.toHexString()
+    entity.collateralId = event.params.collateralId.toHexString()
+    entity.burnDebtAmount = event.params.burnDebtAmount
+    entity.repayAmount = event.params.repayAmount
+    entity.discount = event.params.discount
     entity.block = event.block.number
     entity.timestamp = event.block.timestamp
     entity.save()
     updateAPY(event, event.params.collateralId, 'LIQUIDATE', entity.debtId == entity.collateralId)
-    let userAction = new UserActionEntity(event.transaction.hash.toHex())
+    let userAction = new UserActionEntity(event.transaction.hash.toHexString())
     userAction.action = "LIQUIDATE"
-    userAction.user = event.params.user.toHex()
-    userAction.liquidator = event.params.liquidator.toHex()
-    userAction.ceCollateralAmount = event.params.ceCollateralAmount
-    userAction.debtId = event.params.debtId.toHex()
-    userAction.collateralId = event.params.collateralId.toHex()
-    userAction.debtAmount = event.params.debtAmount
-    userAction.ceDebtAmount = event.params.ceDebtAmount
+    userAction.user = event.params.user.toHexString()
+    userAction.liquidator = event.params.liquidator.toHexString()
+    userAction.collateralAmount = event.params.collateralAmount
+    userAction.debtId = event.params.debtId.toHexString()
+    userAction.collateralId = event.params.collateralId.toHexString()
+    userAction.burnDebtAmount = event.params.burnDebtAmount
+    userAction.repayAmount = event.params.repayAmount
     userAction.block = event.block.number
     userAction.timestamp = event.block.timestamp
     userAction.save()
@@ -102,17 +102,16 @@ export function handleLiquidate(event: Liquidate): void {
 }
 
 export function handleBorrow(event: Borrow): void {
-    let id = event.transaction.hash.toHex()
+    let id = event.transaction.hash.toHexString()
     let entity = new BorrowEntity(id)
     entity.debtAmount = event.params.debtAmount
     entity.tokenAmount = event.params.tokenAmount
     entity.block = event.block.number
     entity.timestamp = event.block.timestamp
-    // @ts-ignore
-    entity.chainId = BigInt.fromI32(event.params.chainId as i32)
-    entity.key = event.params.id.toHex()
-    entity.to = event.params.to.toHex()
-    entity.user = event.params.user.toHex()
+    entity.chainId = event.params.chainId.toHexString()
+    entity.key = event.params.id.toHexString()
+    entity.to = event.params.to.toHexString()
+    entity.user = event.params.user.toHexString()
     entity.save()
 
     let usersEntity = BorrowUsersEntity.load("1");
@@ -132,44 +131,41 @@ export function handleBorrow(event: Borrow): void {
         usersEntity.timestamp = entity.timestamp
         usersEntity.save()
     }
-    updateAPY(event, event.params.id, 'BORROW', entity.chainId.equals(rootChainId))
+    updateAPY(event, event.params.id, 'BORROW', entity.chainId == rootChainId)
 
     let userAction = new UserActionEntity(id)
     userAction.action = "BORROW"
     userAction.debtAmount = event.params.debtAmount
     userAction.tokenAmount = event.params.tokenAmount
     userAction.block = event.block.number
-    userAction.to = event.params.to.toHex()
+    userAction.to = event.params.to.toHexString()
     userAction.timestamp = event.block.timestamp
-    // @ts-ignore
-    userAction.chainId = BigInt.fromI32(event.params.chainId as i32)
-    userAction.key = event.params.id.toHex()
-    userAction.user = event.params.user.toHex()
+    userAction.chainId = event.params.chainId.toHexString()
+    userAction.key = event.params.id.toHexString()
+    userAction.user = event.params.user.toHexString()
     userAction.save()
 }
 
 export function handleDeposit(event: Deposit): void {
-    let id = event.transaction.hash.toHex()
+    let id = event.transaction.hash.toHexString()
     let entity = new DepositEntity(id)
     entity.tokenAmount = event.params.tokenAmount
     entity.ceAmount = event.params.ceAmount
-    // @ts-ignore
-    entity.chainId = BigInt.fromI32(event.params.chainId as i32)
-    entity.key = event.params.id.toHex()
-    entity.user = event.params.user.toHex()
+    entity.chainId = event.params.chainId.toHexString()
+    entity.key = event.params.id.toHexString()
+    entity.user = event.params.user.toHexString()
     entity.timestamp = event.block.timestamp
     entity.block = event.block.number
     entity.save()
-    updateAPY(event, event.params.id, "DEPOSIT", entity.chainId.equals(rootChainId))
+    updateAPY(event, event.params.id, "DEPOSIT", entity.chainId == rootChainId)
 
     let userAction = new UserActionEntity(id)
     userAction.action = "DEPOSIT"
     userAction.tokenAmount = event.params.tokenAmount
     userAction.ceAmount = event.params.ceAmount
-    // @ts-ignore
-    userAction.chainId = BigInt.fromI32(event.params.chainId as i32)
-    userAction.key = event.params.id.toHex()
-    userAction.user = event.params.user.toHex()
+    userAction.chainId = event.params.chainId.toHexString()
+    userAction.key = event.params.id.toHexString()
+    userAction.user = event.params.user.toHexString()
     userAction.timestamp = event.block.timestamp
     userAction.block = event.block.number
     userAction.save()
@@ -177,57 +173,53 @@ export function handleDeposit(event: Deposit): void {
 }
 
 export function handleWithdraw(event: Withdraw): void {
-    let id = event.transaction.hash.toHex()
+    let id = event.transaction.hash.toHexString()
     let entity = new WithdrawEntity(id)
     entity.tokenAmount = event.params.tokenAmount
     entity.ceAmount = event.params.ceAmount
-    // @ts-ignore
-    entity.chainId = BigInt.fromI32(event.params.chainId as i32)
-    entity.key = event.params.id.toHex()
-    entity.user = event.params.user.toHex()
-    entity.to = event.params.to.toHex()
+    entity.chainId = event.params.chainId.toHexString()
+    entity.key = event.params.id.toHexString()
+    entity.user = event.params.user.toHexString()
+    entity.to = event.params.to.toHexString()
     entity.timestamp = event.block.timestamp
     entity.block = event.block.number
     entity.save()
-    updateAPY(event, event.params.id, "WITHDRAW", entity.chainId.equals(rootChainId))
+    updateAPY(event, event.params.id, "WITHDRAW", entity.chainId == rootChainId)
 
     let userAction = new UserActionEntity(id)
     userAction.action = "WITHDRAW"
-    userAction.to = event.params.to.toHex()
+    userAction.to = event.params.to.toHexString()
     userAction.tokenAmount = event.params.tokenAmount
     userAction.ceAmount = event.params.ceAmount
-    // @ts-ignore
-    userAction.chainId = BigInt.fromI32(event.params.chainId as i32)
-    userAction.key = event.params.id.toHex()
-    userAction.user = event.params.user.toHex()
+    userAction.chainId = event.params.chainId.toHexString()
+    userAction.key = event.params.id.toHexString()
+    userAction.user = event.params.user.toHexString()
     userAction.timestamp = event.block.timestamp
     userAction.block = event.block.number
     userAction.save()
 }
 
 export function handleRepay(event: Repay): void {
-    let id = event.transaction.hash.toHex()
+    let id = event.transaction.hash.toHexString()
     let entity = new RepayEntity(id)
     entity.tokenAmount = event.params.tokenAmount
     entity.ceAmount = event.params.ceAmount
     entity.debtAmount = event.params.debtAmount
-    // @ts-ignore
-    entity.chainId = BigInt.fromI32(event.params.chainId as i32)
-    entity.key = event.params.id.toHex()
-    entity.user = event.params.user.toHex()
+    entity.chainId = event.params.chainId.toHexString()
+    entity.key = event.params.id.toHexString()
+    entity.user = event.params.user.toHexString()
     entity.timestamp = event.block.timestamp
     entity.block = event.block.number
     entity.save()
-    updateAPY(event, event.params.id, "REPAY", entity.chainId.equals(rootChainId))
+    updateAPY(event, event.params.id, "REPAY", entity.chainId == rootChainId)
     let userAction = new UserActionEntity(id)
     userAction.action = "REPAY"
     userAction.tokenAmount = event.params.tokenAmount
     userAction.ceAmount = event.params.ceAmount
     userAction.debtAmount = event.params.debtAmount
-    // @ts-ignore
-    userAction.chainId = BigInt.fromI32(event.params.chainId as i32)
-    userAction.key = event.params.id.toHex()
-    userAction.user = event.params.user.toHex()
+    userAction.chainId = event.params.chainId.toHexString()
+    userAction.key = event.params.id.toHexString()
+    userAction.user = event.params.user.toHexString()
     userAction.timestamp = event.block.timestamp
     userAction.block = event.block.number
     userAction.save()
